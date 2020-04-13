0cd4a2f4c1ab90e20b226b095559a9044ce63ba0
Derek <nightwing917@hotmail.com>
2020-04-12 20:34:18 -0600

    added toggle to seats

--- script.js
+++ script.js
@@ -10,5 +10,7 @@ const ticketPrice = +movieSelect.value;
 
 
 container.addEventListener('click', (e) => {
-    console.log(e.target);
+    if(e.target.classList.contains('seat') && !e.target.classList.contains('occupied')) {
+       e.target.classList.toggle('selected');
+    }
 });
