6a9a35267f54c026e3790dd56dec3967b0bbc4e3
Derek <nightwing917@hotmail.com>
2020-04-30 19:09:16 -0600

    Movie seats completed for now

--- script.js
+++ script.js
@@ -43,6 +43,20 @@ function updateSelectedCount() {
 // Get data from local storage and populate UI
 function populateUI() {
    const selectedSeats = JSON.parse(localStorage.getItem('selectedSeats'));
+   
+   if(selectedSeats !== null && selectedSeats.length > 0) {
+      seats.forEach((seat, index) => {
+         if(selectedSeats.indexOf(index) > -1) {
+            seat.classList.add('selected');
+         }
+      });
+   }
+
+   const selectedMovieIndex = localStorage.getItem('selectedMovieIndex');
+
+   if(selectedMovieIndex !== null) {
+      movieSelect.selectedIndex = selectedMovieIndex;
+   }
 }
 
 
@@ -63,3 +77,8 @@ container.addEventListener('click', (e) => {
        updateSelectedCount();
     }
 });
+
+
+
+// Initial count and total set
+updateSelectedCount();
